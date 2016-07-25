83992e101a20ca8828a5c55b07cf0cc3cbe0b711
wangmingsen <254949825@qq.com>
2016-07-25 07:17:54 +0000

    定时任务修改

--- src/main/java/com/winxuan/config/SchedulingConfig.java
+++ src/main/java/com/winxuan/config/SchedulingConfig.java
@@ -5,10 +5,12 @@ package com.winxuan.config;
  * Created by wangmingsen on 2016/7/5.
  */
 
+import com.winxuan.support.KettleExecu;
 import com.winxuan.support.MagazineSync;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,17 +20,32 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class SchedulingConfig {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Value("${kittle.kjb.file}")
+    String kittleKjbFile;
+
+
 
     @Autowired
     MagazineSync magazineSync;
-
+    @Autowired
+    KettleExecu kettleExecu;
+    /**
+     * 中邮期刊数据同步
+     */
     @Scheduled(cron = "0 0 0 0/2 * ?") // 每2天执行一次
     public void scheduler() {
-        logger.info(">>>>>>>>>>>>> Start timing task ! ... ");
+        logger.info("中邮数据同步 start 》》》》》");
         magazineSync.callRpc();
+        logger.info("中邮数据同步 end 》》》》》");
     }
-    @Scheduled(cron = "0 0/2 * * * ?") // 每2分钟执行一次
-    public void partScheduler() {
-        logger.info(">>>>>>>>>>>>> elib_sync 程序正常运行 ... ");
+
+    /**
+     * 同步到统计平台
+     */
+    @Scheduled(cron = "0 0 0/2 * * ?") // 每1小时执行一次
+    public void behaviorRecordJob() {
+        logger.info("到统计平台开始同步》》》》》");
+        kettleExecu.runJob(kittleKjbFile);
+        logger.info("到统计平台记录同步完成》》》》》");
     }
 }
--- src/main/java/com/winxuan/support/KettleExecu.java
+++ src/main/java/com/winxuan/support/KettleExecu.java
@@ -1,6 +1,8 @@
 package com.winxuan.support;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.exception.KettleXMLException;
@@ -8,6 +10,7 @@ import org.pentaho.di.job.Job;
 import org.pentaho.di.job.JobMeta;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,33 +18,38 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class KettleExecu {
+    @Value("${kittle.filePath}")
+    String filePath;
 
-
-    public static void runJob(String filePath) {
+    public static final Log LOG = LogFactory.getLog(KettleExecu.class);
+    public  void runJob(String file) {
+        System.out.println(filePath);
         try {
             KettleEnvironment.init();
             // jobname 是Job脚本的路径及名称
-            JobMeta jobMeta = new JobMeta(filePath, null);
+            JobMeta jobMeta = new JobMeta(file, null);
             Job job = new Job(null, jobMeta);
             // 向Job 脚本传递参数，脚本中获取参数值：${参数名}
             // job.setVariable(paraname, paravalue);
 
+            job.setVariable("file_path",filePath);
             job.start();
             job.waitUntilFinished();
             if (job.getErrors() > 0) {
-                System.out.println("decompress fail!");
+                LOG.error("decompress fail!");
             }
         } catch (KettleException e) {
             System.out.println(e);
         }
     }
 
-    // 调用Transformation示例：
-    public static void runTrans(String filePath) {
+    // 调用Transformation：
+    public void runTrans(String file) {
         try {
             KettleEnvironment.init();
-            TransMeta transMeta = new TransMeta(filePath);
+            TransMeta transMeta = new TransMeta(file);
             Trans trans = new Trans(transMeta);
+            trans.setVariable("file_path",filePath);
             trans.prepareExecution(null);
             trans.startThreads();
             trans.waitUntilFinished();
@@ -49,16 +57,12 @@ public class KettleExecu {
 
             }
         } catch (KettleXMLException e) {
-            e.printStackTrace();
+            LOG.error(e);
         } catch (KettleException e) {
-            e.printStackTrace();
+            LOG.error(e);
         }
     }
 
-    public void kettleDispatch(String filePath){
-
-
-    }
 
 
 }
